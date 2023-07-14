ffdbe565f6f1cc1c5a72a4b2adf98234f8282677
muro906 <mainamillicent68@gmail.com>
2023-07-14 05:03:21 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main-entry point
+ * puts-print string followed by new line
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
