76ef3de63a9fd438fc9fe74fbde2d401ea369dc8
muro906 <mainamillicent68@gmail.com>
2023-07-14 05:53:02 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * main-entry point
+ * write-write to stderr
+ * strlen-length of string
+ * Return: always 1
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(2, str, strlen(str));
+	return (1);
+}
