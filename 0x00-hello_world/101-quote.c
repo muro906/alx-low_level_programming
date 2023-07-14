421a2ff9228d32456a8a21766785328a71008df5
muro906 <mainamillicent68@gmail.com>
2023-07-14 06:17:36 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,6 +13,6 @@ int main(void)
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	, strlen(str));
+	, strlen(str) + 1);
 	return (1);
 }
