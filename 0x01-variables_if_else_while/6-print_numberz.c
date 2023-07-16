30c3c4a90bbbb2ec3c2c91ae8d3b96d1887e220d
muro906 <mainamillicent68@gmail.com>
2023-07-16 10:53:53 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main- entry point for program
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0;  c <= 9; c++)
+	{
+		putchar(c + '0');
+	}
+	putchar('\n');
+	return (0);
+}
