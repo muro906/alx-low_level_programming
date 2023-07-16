329b06337a508abb4eeb0825e3cf0a2366bc3fca
muro906 <mainamillicent68@gmail.com>
2023-07-16 10:30:11 -0700

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main- entry point for program
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97;  c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
