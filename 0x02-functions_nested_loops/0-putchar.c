5a8dbd1e7d7d2e7de2351ce4cbe14fa479812137
muro906 <mainamillicent68@gmail.com>
2023-07-18 04:51:10 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,6 @@
 /**
  * main- Entry point for the program
  * _putchar- prints a character to stdout
- * @c: argument
  * Return: always 0
  */
 int main(void)
@@ -17,5 +16,6 @@ int main(void)
 		_putchar(a[0]);
 		i++;
 	}
+	_putchar('\n')
 	return (0);
 }
