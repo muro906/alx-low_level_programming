8b8ebf8dc58f6a156a77d1254b0d10afc535572d
muro906 <mainamillicent68@gmail.com>
2023-07-18 04:45:46 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * main- Entry point for the program
+ * _putchar- prints a character to stdout
+ * @c: argument
+ * Return: always 0
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int i;
+
+	i = 0;
+	while (i != '\0')
+	{
+		_putchar(a[0]);
+		i++;
+	}
+	return (0);
+}
