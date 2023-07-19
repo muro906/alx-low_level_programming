cf6722f3696557dd286fa72272ef0e8c400c4b38
muro906 <mainamillicent68@gmail.com>
2023-07-19 10:23:58 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,13 +17,13 @@ void jack_bauer(void)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					if (a >= 2 && b >= 4)
+					if (i >= 2 && j >= 4)
 						break;
-					_putchar(a + '0');
-					_putchar(b + '0');
+					_putchar(i + '0');
+					_putchar(j + '0');
 					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
+					_putchar(k + '0');
+					_putchar(l + '0');
 					_putchar('\n');
 				}
 			}
