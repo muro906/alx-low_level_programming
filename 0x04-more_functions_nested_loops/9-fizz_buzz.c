fcb91b5188319185456bf5f7f1512d1104888b1b
muro906 <mainamillicent68@gmail.com>
2023-07-21 10:57:53 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include"main.h"
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz ");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i == 1)
+			printf("%d ", i);
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
