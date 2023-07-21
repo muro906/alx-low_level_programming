25d94c0a2cb6cd546d45aa814cc488567b7fd7b8
muro906 <mainamillicent68@gmail.com>
2023-07-21 01:43:26 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _isupper-print if uppercase
+ * Return: always 0 or 1
+ * @c: parameter
+ */
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
