3e6cc6f37cba80ba907240ebdfac7d7d4d4f5cfd
muro906 <mainamillicent68@gmail.com>
2023-08-24 12:52:11 +0300

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include"lists.h"
+/**
+ * print_list-Print the elements in a node
+ * @h: address of the head node
+ * Return: no of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *ptr = h;
+	size_t len = 0;
+
+	if (ptr == NULL)
+		return (0);
+	else
+	{
+		while (ptr != NULL)
+		{
+			if (ptr->str == NULL)
+				printf("[0] (nil)");
+			else
+				printf("[%d] %s", ptr->len, ptr->str);
+			ptr = ptr->next;
+			len++;
+		}
+	}
+	return (len);
+}
