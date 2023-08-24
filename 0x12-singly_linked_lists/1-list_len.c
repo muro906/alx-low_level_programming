e931fb455909699694139bb42c67e6bbdbbc1aac
muro906 <mainamillicent68@gmail.com>
2023-08-24 17:37:55 +0300

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+/**
+ * list_len-Returns number of nodes in the linked list
+ * @h: Address of the head node
+ * Return: The number of nodes in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	list_t *ptr = (list_t*) h;
+	size_t num = 0;
+	unsigned long int len = 0;
+
+	/**if (ptr == NULL)
+		return (0);
+		*/
+	while (!ptr)
+	{
+		ptr = ptr->next;
+		num++;
+	}
+	len = (unsigned long int) num;
+	return (len);
+}
