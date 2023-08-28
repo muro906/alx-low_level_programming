b82ff96f05b36484b93b5bc12bc5c7120f5192e0
muro906 <mainamillicent68@gmail.com>
2023-08-29 01:17:44 +0300

    fixing betty errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	if (new_dog ==  NULL)
+	if (new_dog ==	NULL)
 		return (NULL);
 	new_dog->name = malloc(_strlen(name) + 1);
 	if (new_dog->name == NULL)
