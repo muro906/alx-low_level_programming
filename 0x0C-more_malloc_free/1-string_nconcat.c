b98ac8ed9d310bdbf9ba93e35996fe53d8dab902
muro906 <mainamillicent68@gmail.com>
2023-08-29 11:12:46 +0300

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,8 +9,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int i;
-	unsigned len1 = 0, len2 = 0;
+	unsigned int len1 = 0, len2 = 0;
 	unsigned int j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1 && s1[len1])
 		len1++;
