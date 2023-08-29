9e78d2f34ad2f6cd118eb6892409cd7064e3ae2e
muro906 <mainamillicent68@gmail.com>
2023-08-29 17:05:59 +0300

    Tash 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,15 +16,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		ptr =  malloc(new_size);
 		return (ptr);
-	}/**case when ptr is NULL and rest aren't*/
+	} /**case when ptr is NULL and rest aren't*/
 	else if (new_size == old_size)/*Case when nothing is allocated or freed*/
 		return (ptr);
 	else if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
-	}/*When new is equal to zero then free*/
-	new = malloc(old_size + new_size);
+	} /*When new is equal to zero then free*/
+	new = malloc(new_size);
 	if (new == NULL)
 	{
 		free(new);
