c0aabf3c0904528c07a71cc51eefe03c3c409569
muro906 <mainamillicent68@gmail.com>
2023-08-29 17:36:00 +0300

    Task 0 - task 8

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include<main.h>
+/**
+ * _puts_recursion: Print string recursively
+ * @s: string argument
+ */
+void _puts_recursion(char *s)
+{
+	if (*s )
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
