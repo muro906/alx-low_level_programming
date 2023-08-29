62393e620f07ec78e9f5e92f97cbe7cfb7cc10d0
muro906 <mainamillicent68@gmail.com>
2023-08-29 17:41:55 +0300

    fixing Betty errors

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include"main.h"
 /**
- * _puts_recursion: Print string recursively
+ * _puts_recursion- Print string recursively
  * @s: string argument
  */
 void _puts_recursion(char *s)
 {
-	if (*s )
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
