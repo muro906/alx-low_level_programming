043a70b7b6eac191a43c1984198f3f00d533eae2
muro906 <mainamillicent68@gmail.com>
2023-08-29 11:46:55 +0300

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include"main.h"
+/**
+ * _calloc-Allocates memory in blocks
+ * @nmeb: Number of array members
+ * @size: Size of each block
+ * Return: Void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	
+	if(size == 0 || nmemb == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, size * nmemb);
+	return (ptr);
+}
+
+/**
+ * _memset- Fill memory with a constant byte
+ * @str: pointer argument
+ * @c: value to be filled
+ * @size: Number of time to be filled
+ * Return: char pointer
+ */
+char *_memset(char *str, char c, unsigned int size)
+{
+	unsigned int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (; i < size; i++)
+	{
+		str[i] = c;
+	}
+	return (str);
+}
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 #include<stdio.h>
 #include<stdlib.h>
+char *_memset(char *str, char c, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
